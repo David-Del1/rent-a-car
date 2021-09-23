7d4b719c40ed5ca9a01e9d60ebeba58618252c97
David Delgadillo <daviddelgadillo@davids-mbp.lan>
2021-09-23 16:10:32 -0700

    installed react-calendar to use for booking functionality on landing page. styled calendars and got them to work as desired

--- src/app/components/bookCard/index.tsx
+++ src/app/components/bookCard/index.tsx
@@ -1,13 +1,18 @@
-import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react';
+import { faCalendarAlt, faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import tw from 'twin.macro';
 import Button from '../Button';
+import 'react-calendar/dist/Calendar.css';
+import Calendar from 'react-calendar';
+import { Marginer } from '../Margin';
+import { SCREENS } from '../responsive';
 
 const CardContainer = styled.div`
   min-height: 4.3em;
-  box-shadow: 0 1.3px 12px -3px rgba(0, 0, 0, 0.4); 
+  box-shadow: 0 1.3px 12px -3px rgba(0, 0, 0, 0.4);
+  margin-bottom: 20em;
   ${tw `
     flex
     justify-center
@@ -28,6 +33,7 @@ const CardContainer = styled.div`
 const ItemsContainer = styled.div`
   ${tw`
     flex
+    relative
   `}
 `
 
@@ -42,11 +48,23 @@ const Icon = styled.span`
   `}
 `;
 
+const SmallIcon = styled.span`
+  ${tw `
+    text-gray-600
+    fill-current
+    text-xs
+    md:text-base
+    ml-1
+  `}
+`
+
 const Name = styled.span`
   ${tw`
     text-gray-600
     text-xs
     md:text-sm
+    cursor-pointer
+    select-none
   `}
 `;
 
@@ -62,22 +80,78 @@ height: 45%;
   `}
 `;
 
+const CalendarContainer = styled(Calendar)`
+  position: absolute;
+  max-width: none;
+  user-select: none;
+  top: 3em;
+  left: 0em;
+
+  ${({ offset }: any) => offset && css`
+    left: -6em
+  `}
+
+  @media only screen and (min-width: ${SCREENS.md}) {
+    top: 3.5em;
+    left: -2em;
+  }
+` as any;
+
 function BookCard() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [startCalendarOpen, setStartCalendarOpen] = useState(false);
+  const [returnDate, setReturnDate] = useState(new Date());
+  const [returnCalendarOpen, setReturnCalendarOpen] = useState(false);
+
+  const handleOpenStartCalendar = () => {
+    setStartCalendarOpen(!startCalendarOpen)
+    setReturnCalendarOpen(false);
+  }
+
+  const handleOpenReturnCalendar = () => {
+    setReturnCalendarOpen(!returnCalendarOpen);
+    setStartCalendarOpen(false);
+  }
+  
   return (
     <CardContainer>
       <ItemsContainer>
         <Icon>
          <FontAwesomeIcon icon={faCalendarAlt} />
         </Icon>
-        <Name>Pick Up Date</Name>
+        <Name
+          onClick={handleOpenStartCalendar}
+        >
+          Pick Up Date
+        </Name>
+        <SmallIcon>
+          <FontAwesomeIcon icon={startCalendarOpen ? faCaretUp : faCaretDown } />
+        </SmallIcon>
+        {startCalendarOpen && (
+        <CalendarContainer value={startDate} onChange={setStartDate as any} />
+        )}
       </ItemsContainer>
       <LineSeparator />
-      <ItemsContainer className="mr-5">
+      <ItemsContainer className="mr-6">
         <Icon>
          <FontAwesomeIcon icon={faCalendarAlt} />
         </Icon>
-        <Name>Return Date</Name>
+        <Name
+          onClick={handleOpenReturnCalendar}
+        >
+          Return Date
+        </Name>
+        <SmallIcon>
+          <FontAwesomeIcon icon={returnCalendarOpen ? faCaretUp : faCaretDown } />
+        </SmallIcon>
+        {returnCalendarOpen && (
+        <CalendarContainer 
+          offset
+          value={returnDate} 
+          onChange={setReturnDate as any} />
+        )}
       </ItemsContainer>
+      <Marginer direction="horizontal" margin="2em" />
       <Button theme="filled" text="Book Your Ride" />
     </CardContainer>
   );
--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components';
 import tw from 'twin.macro';
 import BookCard from '../../app/components/bookCard';
+import { Marginer } from '../../app/components/Margin';
 import NavBar from '../../app/components/NavBar';
 import TopSection from './TopSection';
 
@@ -20,6 +21,7 @@ export function HomePage() {
   return <PageContainer>
     <NavBar />
     <TopSection />
+    <Marginer direction="vertical" margin="1em" />
     <BookCard />
   </PageContainer>
 }
